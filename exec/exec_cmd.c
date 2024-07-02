06d4d5aef9c2f78a26f0c3078aa08833af68ff58
NilsLeVrai <nilslucas@wanadoo.fr>
2024-07-02 13:47:22 +0200

    Nils : 02/07 tranquille2

--- exec/exec_cmd.c
+++ exec/exec_cmd.c
@@ -6,11 +6,23 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:54:36 by niabraha          #+#    #+#             */
-/*   Updated: 2024/07/02 11:54:49 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/07/02 13:47:02 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+// mon main de test
+
+int main()
+{
+	t_ms bite;
+
+	bite.content = "ls";
+	bite.token = 2;
+	ft_printf("%d\n", bite.token);
+	ft_printf("%s\n", bite.content);
+}
 
 /* notes :
 Builtin pwd, env echo en first (conseil de Max)
--- utils/execution.c
+++ utils/execution.c
@@ -6,20 +6,8 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:50:11 by niabraha          #+#    #+#             */
-/*   Updated: 2024/07/02 13:42:11 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/07/02 13:46:48 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-# include <stdio.h> // a enlever
-// mon main de test
-
-int main()
-{
-	t_ms bite;
-
-	bite.content = "ls";
-	bite.token = 2;
-	printf("%d\n", bite.token);
-	printf("%s\n", bite.content);
-}
