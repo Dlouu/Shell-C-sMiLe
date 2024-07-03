83000d59f77683e84a48e54db0542e8e0eee9537
Dlouu <135530996+Dlouu@users.noreply.github.com>
2024-07-03 13:32:59 +0200

    03/07 FILE changed to FILENAME

--- inc/typedefs.h
+++ inc/typedefs.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   typedefs.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mbaumgar <mbaumgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 16:15:01 by mbaumgar          #+#    #+#             */
-/*   Updated: 2024/07/03 13:25:41 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/07/03 13:32:26 by mbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ typedef enum e_token
 	REDIR_DOUBLE_LEFT,
 	REDIR_DOUBLE_RIGHT,
 	PIPE,
-	FILEE, // changer le nomde la variable FILE? cree des bugs chez moi, FILE est un terme deja utilise
+	FILENAME,
 }	t_token;
 
 #endif
