5e792af5e9edbafdb81d2f86030a72272f26fee4
NilsLeVrai <nilslucas@wanadoo.fr>
2024-07-08 20:34:37 +0200

    Nils : echo pas degeu, on dirait ca fonctionnne o_O

--- builtin/echo.c
+++ builtin/echo.c
@@ -6,56 +6,47 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:49:06 by niabraha          #+#    #+#             */
-/*   Updated: 2024/07/04 19:21:41 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/07/08 19:16:29 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-valide + valide  echo -n -n bite (bite sans saut de ligne)
-valide + pas valide echo -n -n5 bite (-n5 bite sans saut de ligne)
-pas valide + valide saut de ligne
-pas valide + pas valide
-
-tant que y'a -n ou -nnnn je skip, j'ecris des que ca change. sans \name
-sinon jecris tout
-*/
-
-
 #include "../inc/minishell.h"
+
+static int	is_flag_n(const char *content)
+{
+	int	i;
+
+	i = 1;
+	if (content[0] != '-')
+		return (0);
+	while (content[i] == 'n')
+		i++;
+	return (content[i] == '\0');
+}
 
 int	ft_echo(t_ms **lst)
 {
-	int		n_flag;
-	t_ms	*tmp;
+	t_ms	*current;
+	int		flag_n;
 
-	n_flag = 0;
-	tmp = (*lst)->next; // skip echo a enlever plus tard
-	while (tmp != NULL)
+	current = *lst;
+	current = (*lst)->next; // skip "echo" --> a enlever plus tard
+	flag_n = 0;
+	while (current && current->content)
 	{
-		if (tmp->content[0] == '-')
-		{
-			int i = 1;
-			while (tmp->content[i] == 'n')
-				i++;
-			if (tmp->content[i] == '\0')
-				n_flag = 1;
-		}
-		else
-		{
-			write(1, tmp->content, ft_strlen(tmp->content));
-			if (tmp->next != NULL)
-				write(1, " ", 1);
-		}
-		tmp = tmp->next;
+		if (!is_flag_n(current->content))
+			break ;
+		current = current->next;
+		flag_n = 1;
 	}
-	if (n_flag == 0)
+	while (current && current->content)
+	{
+		write(1, current->content, ft_strlen(current->content));
+		current = current->next;
+		if (current && current->content)
+			write(1, " ", 1);
+	}
+	if (!flag_n)
 		write(1, "\n", 1);
 	return (0);
 }
-
-/* notes :
-Faire attention echo -nnnnn -nnnlol -n lol
-utiliser des write plutot que des printf (conseil de Max)
-echo -nnnn5 -n lol
-echo -nn5 -n -nnn5 lol
-*/
