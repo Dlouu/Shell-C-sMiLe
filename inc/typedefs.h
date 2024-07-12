deebba1e18fef40f166d82805661e20549515339
Dlouu <135530996+Dlouu@users.noreply.github.com>
2024-07-12 18:49:52 +0200

    Dlou : 12/07 env start

--- inc/typedefs.h
+++ inc/typedefs.h
@@ -6,7 +6,7 @@
 /*   By: mbaumgar <mbaumgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 16:15:01 by mbaumgar          #+#    #+#             */
-/*   Updated: 2024/07/10 15:21:02 by mbaumgar         ###   ########.fr       */
+/*   Updated: 2024/07/12 18:47:14 by mbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,30 @@ typedef enum e_token
 	REDIR_DOUBLE_RIGHT,
 	PIPE,
 	FILENAME,
+}	t_token_code;
+
+typedef struct s_token //nom temporaire de test
+{
+	char			*content; //cmd etc.
+	int				type; // (des entiers) COMMAND, BUILTIN, ARG, ...
+	int				index; // echo -n -nnn. echo = 0, -n = 1, -nnn = 2
+	int				builtin; // 1 si builtin, 0 sinon
+	struct s_token	*next;
+	struct s_token	*prev;
 }	t_token;
 
 typedef struct s_env
 {
-	char			*key;
-	char			*value;
+	char			*key; //PATH
+	char			*value; // /usr/bin:/bin:/usr/sbin:/sbin
+	struct s_env	*next;
 }	t_env;
+
+typedef struct s_ms
+{
+	struct s_env	*env;
+	struct s_token	*token;
+	int				exit_code;
+}	t_ms;
 
 #endif
--- minishell.c
+++ minishell.c
@@ -6,32 +6,79 @@
 /*   By: mbaumgar <mbaumgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 15:42:53 by mbaumgar          #+#    #+#             */
-/*   Updated: 2024/07/10 16:13:24 by mbaumgar         ###   ########.fr       */
+/*   Updated: 2024/07/12 18:46:00 by mbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/minishell.h"
 
+// int	find_index(char *str, char c)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (str[i] && str[i] != c)
+// 		i++;
+// 	return (i);
+// }
+
+// void	lstdelone_env(t_env *env, void (*del)(void *))
+// {
+// 	if (!env || !del)
+// 		return ;
+// 	(*del)(env->key);
+// 	(*del)(env->value);
+// 	wfree(env);
+// }
+
+// void	lstadd_env(t_env **env, t_env *new)
+// {
+// 	new->next = *env;
+// 	*env = new;
+// }
+
+// t_env	*lstnew_env(char *key, char *value)
+// {
+// 	t_env	*new;
+
+// 	new = (t_env *)walloc(sizeof(t_env), TRUE);
+// 	if (!new)
+// 		return (NULL);
+// 	new->key = key;
+// 	new->value = value;
+// 	new->next = NULL;
+// 	return (new);
+// }
+
+// t_env	*get_envv(t_ms *ms, char **env)
+// {
+// 	t_env	*env_lst;
+// 	int		i;
+// 	int		equals;
+// 	int		len;
+// 	t_env	*new;
+
+// 	env_lst = (t_env *)walloc(sizeof(t_env), TRUE);
+// 	i = 0;
+// 	while (env[i])
+// 	{
+// 		len = ft_strlen(env[i]);
+// 		equals = find_index(env[i], '=');
+// 		new = lstnew_env(ft_substr(env[i], 0, equals, TRUE), \
+// 		ft_substr(env[i], equals + 1, len, TRUE));
+// 		lstadd_env(&env_lst, new);
+// 		i++;
+// 	}
+// 	return (*env);
+// }
+
 // int	main(int argc, char **argv, char **env)
 // {
+// 	t_ms	*ms;
+
+// 	ms = walloc(sizeof(t_ms), TRUE);
 // 	(void)argc;
 // 	(void)argv;
-// 	(void)env;
-// 	char **tab = walloc(sizeof(char *) * 4, 1);
-// 	tab[0] = ft_strdup("hello", 1);
-// 	tab[1] = ft_strdup("world", 1);
-// 	tab[2] = ft_strdup("42", 1);
-// 	tab[3] = NULL;
-// 	char **tab2 = walloc(sizeof(char *) * 4, 0);
-// 	tab2[0] = ft_strdup("ji", 0);
-// 	tab2[1] = ft_strdup("pepey", 0);
-// 	tab2[2] = NULL;
-// 	ft_printf("%s\n", tab[0]);
-// 	ft_printf("%s\n", tab2[0]);
-// 	wfree(tab[0]);
-// 	wclear(0);
-// 	printf("%s\n", tab2[1]);
-// 	printf("%s\n", tab[1]);
-// 	printf("%s\n", tab[2]);
+// 	get_envv(ms, env);
 // 	return (0);
 // }
