bf13b16fe699a6dc435f56095cf80dfa8a67ad55
NilsLeVrai <nilslucas@wanadoo.fr>
2024-07-12 16:01:41 +0200

    Nils : 12/07 starting env

--- builtin/env.c
+++ builtin/env.c
@@ -6,11 +6,16 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:49:09 by niabraha          #+#    #+#             */
-/*   Updated: 2024/07/02 20:07:03 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/07/12 15:41:56 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+
+/*
+regarder les typesdef
+*/
 
 int	ft_env(t_ms **lst, char **envp)
 {
--- builtin/exit.c
+++ builtin/exit.c
@@ -6,10 +6,9 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:49:12 by niabraha          #+#    #+#             */
-/*   Updated: 2024/07/03 16:46:04 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/07/12 15:14:59 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-// Marie ^^
