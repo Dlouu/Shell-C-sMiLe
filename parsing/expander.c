7820c42063acc7bb22309d8e0bfdbf8357eaef21
Dlouu <135530996+Dlouu@users.noreply.github.com>
2024-08-02 02:11:50 +0200

    Dlou : 02/08 expand syntax + exit code

--- parsing/expander.c
+++ parsing/expander.c
@@ -6,11 +6,58 @@
 /*   By: mbaumgar <mbaumgar@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 16:06:12 by mbaumgar          #+#    #+#             */
-/*   Updated: 2024/08/01 22:19:42 by mbaumgar         ###   ########.fr       */
+/*   Updated: 2024/08/02 02:08:20 by mbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+void	expand_var(t_ms *ms, t_token *tk)
+{
+	(void)tk;
+	(void)ms;
+	printf("si var trouvé, faut mettre le env ici\n");
+	printf("on resplit si nécessaire\n");
+}
+
+void	expand_var_dquoted(t_ms *ms, t_token *tk)
+{
+	(void)tk;
+	(void)ms;
+	printf("si var trouvé, faut mettre le env ici\n");
+	printf("mais on resplit pas\n");
+}
+
+//fautdra break par bloc de $$
+//$$$ = pid + $
+//$$ = pid
+//$$$$ = pid + pid
+void	expand_pid_number(t_ms *ms, t_token *tk)
+{
+	(void)tk;
+	(void)ms;
+	printf("$$ trouvé, faut mettre le pid ici\n");
+}
+
+void	expand_exit_code(t_ms *ms, t_token *tk, int *i)
+{
+	char	*exit_code;
+	char	*left;
+	char	*right;
+	char	*temp;
+	size_t	len;
+
+	temp = tk->content;
+	len = ft_strlen(tk->content) - *i - 2;
+	left = ft_substr(tk->content, 0, *i, FALSE);
+	exit_code = ft_itoa(ms->exit_code, FALSE);
+	right = ft_substr(tk->content, *i + 2, len, FALSE);
+	tk->content = ft_free_strjoin(left, exit_code, FALSE);
+	tk->content = ft_strjoin(tk->content, right, FALSE);
+	wfree(temp);
+	wfree(right);
+	*i += ft_strlen(exit_code) - 1;
+}
 
 void	expander(t_ms *ms)
 {
@@ -20,34 +67,22 @@ void	expander(t_ms *ms)
 	tk = ms->token_lexed;
 	while (tk)
 	{
-		i = 0;
-		while (tk->content[i])
+		i = -1;
+		while (tk->content[++i])
 		{
 			if (tk->content[i] == '$' && tk->squote == 0)
 			{
-				printf("$ trouvé\n");
-				//compter le nombre de $ car faut repasser plusieurs fois
 				if (tk->content[i + 1] == '?')
-				{
-					printf("? trouvé, faut mettre le exit code ici\n");
-					//wfree, puis itoa, puis ft_strjoin ?
-				}
-				else if (tk->dquote == 1)
-				{
-					printf("dquote avec $, on cherche le env mais on \
-					split pas\n");
-				}
+					expand_exit_code(ms, tk, &i);
 				else if (!tk->content[i + 1])
-				{
-					printf("pas de env à chercher\n");
-				}
+					break ;
+				else if (tk->content[i + 1] == '$')
+					expand_pid_number(ms, tk);
+				else if (tk->dquote == 1)
+					expand_var_dquoted(ms, tk);
 				else
-				{
-					printf("pas de ? alors on cherche le env correspondant, \
-					si on ne trouve pas, on delete le truc\n");
-				}
+					expand_var(ms, tk);
 			}
-			i++;
 		}
 		tk = tk->next;
 	}
