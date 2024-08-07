1ba980a35a6c35a9c3f97a113e422b6d109ecc19
NilsLeVrai <nilslucas@wanadoo.fr>
2024-08-07 20:25:09 +0200

    Nils : cd tranquille

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,27 +6,36 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:49:04 by niabraha          #+#    #+#             */
-/*   Updated: 2024/08/07 18:31:38 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/08/07 20:25:00 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+// printf("arg = %d\n", ms->token_lexed->builtin);
+
 int	ft_cd(t_ms *ms)
 {
-/* 	char *path;
+	char *current_path;
 
-	path = getcwd(NULL, 0);
-	if (!path)
-		return (printf("path unset je crois\n"), 1); */
+	current_path = getcwd(NULL, 0);
+	if (!current_path)
+		return (printf("current_path unset je crois\n"), 1);
+	if (!ms->token_lexed->next || ft_strcmp(ms->token_lexed->next->content, "~") == 0)
+	{
+		if (chdir(getenv("HOME")) == -1)
+			return (printf("cd: HOME not set\n"), 1);
+	}
+	else if (chdir(ms->token_lexed->next->content) == -1)
+		return (printf("minishell: cd: %s: No such file or directory\n", ms->token_lexed->next->content), 1);
 	return (0);
 }
 /* 
-faire un getcwd --> si marche pas --> path unset
+faire un getcwd --> si marche pas --> current_path unset DONE
 on change la valeur de OLDPWD
-si la prochaine valeur == NULL || ~ go faire un cd avec la valeur de HOME
-chdir e la valeur d'apres (on suppose que c'est un chemin, on regarde si c'est valide)
-si marche pas --> chemin non valide
+si la prochaine valeur == NULL || ~ go faire un cd avec la valeur de HOME    DONE
+chdir e la valeur d'apres (on suppose que c'est un chemin, on regarde si c'est valide) DONE
+si marche pas --> chemin non valide DONE
 on change la valeur de PWD
  */
 
@@ -39,8 +48,8 @@ on change la valeur de PWD
 // cd .
 // cd
 // cd ~
-// cd e/abc/e/abc = relative path
-// cd e/abc = absolute path
+// cd e/abc/e/abc = relative current_path
+// cd e/abc = absolute current_path
 // cd split sur les paths et lster un par un les dossiers
 // //setenv("PWD", path_max, 1); ? utile ou pas ?
 // */
