d2cc6f5ef012bdd9130fcc7903a778d937558c16
NilsLeVrai <nilslucas@wanadoo.fr>
2024-08-12 15:39:49 +0200

    Nils : ^^

--- utils/envp.c
+++ utils/envp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   envp.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niabraha <niabraha@student.42mulhouse.f    +#+  +:+       +#+        */
+/*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 16:32:45 by mbaumgar          #+#    #+#             */
-/*   Updated: 2024/08/11 21:47:35 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/08/12 15:11:15 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ void replace_value(t_list *env, char *key, char *value)
 	{
 		if (ft_strcmp(((t_env *)tmp->data)->key, key) == 0)
 		{
-			free(((t_env *)tmp->data)->value);
-			((t_env *)tmp->data)->value = ft_strdup(value, 1);
+			wfree(((t_env *)tmp->data)->value);
+			((t_env *)tmp->data)->value = ft_strdup(value, TRUE);
 			return ;
 		}
 		tmp = tmp->next;
